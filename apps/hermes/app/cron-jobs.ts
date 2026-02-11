52eb795b0a9ab3a28527f64788ee1178b5b8a9ef
budhi <budhiw@protonmail.com>
2026-02-11 18:32:57 +0700

    update cron for every 1 minute

--- apps/hermes/app/cron-jobs.ts
+++ apps/hermes/app/cron-jobs.ts
@@ -5,8 +5,8 @@ export function initCronJobs() {
 
     // Schedule every day at 11:00 AM
     // Format: second (optional), minute, hour, day of month, month, day of week
-    cron.schedule("0 11 * * *", () => {
-        console.log("Hello World");
+    cron.schedule("* * * * *", () => {
+        console.log(`${new Date().toISOString()}: Hello World`);
     });
 
     console.log("Cron job 'Hello World' scheduled for 11:00 AM daily");
